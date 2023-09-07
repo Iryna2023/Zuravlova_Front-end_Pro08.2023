02a98045c10bfb698394b98bc98b05009c0cb0da
Ірина Журавльова <irina5275z@gmail.com>
2023-09-07 15:53:58 +0300

    Changes have been made to page Homework 11. Function with closing.

--- front-end_Pro_d.z_11/js/form.js
+++ front-end_Pro_d.z_11/js/form.js
@@ -1,17 +1,15 @@
-function sum() {
+let sum = (function() {
     let a = 0;
     return function(b) {
         a += b;
         alert(a);
         return a;
     }
-}
-
-let sumResult = sum();
+})();
 
 let userInput =  prompt('Введіть перше число, будь ласка:');
 
-sumResult(Number(userInput));
-sumResult(5);
-sumResult(20);
-sumResult(35);
+sum(Number(userInput));
+sum(5);
+sum(20);
+sum(35);
