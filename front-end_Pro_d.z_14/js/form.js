ce7d935e610b74d8fc04b5b3ce2ee097b96c5488
Ірина Журавльова <irina5275z@gmail.com>
2023-09-15 18:49:36 +0300

    Changes have been made to page Homework 14. Student.

--- front-end_Pro_d.z_14/js/form.js
+++ front-end_Pro_d.z_14/js/form.js
@@ -35,7 +35,7 @@ class Student {
         
     summary() {
         const averageGrade = this.getAverageGrade();
-        const averageAttendance = this.attendance.filter(Boolean).length / this.attendance.filter(item => item !== null).length;
+        const averageAttendance = this.attendance.filter(Boolean).length / this.attendance.length;
 
         if (averageGrade > 90 && averageAttendance > 0.9) {
             alert ('Молодець!');
@@ -49,19 +49,31 @@ class Student {
 }
 
 const student1 = new Student('Сергій', 'Омельченко', 1993, [100, 100, 100, 100, 100]);
-    for (let i = 0; i < 25; i++) student1.present();
-    for (let i = 0; i < 0; i++) student1.absent();
+    for (let i = 0; i < 25; i++) {
+        student1.present();
+    }
+    for (let i = 0; i < 0; i++) {
+        student1.absent();
+    }
     alert(`Студент: ${student1.name} ${student1.surname}, Вік: ${student1.getAge()}`)
     student1.summary();
 
 const student2 = new Student('Ярослав', 'Демченко', 1994, [81, 82, 83, 84, 85]);
-    for (let i = 0; i < 25; i++) student2.present();
-    for (let i = 0; i < 0; i++) student2.absent();
+    for (let i = 0; i < 25; i++) {
+        student2.present();
+    }
+    for (let i = 0; i < 0; i++) {
+        student2.absent();
+    }
     alert(`Студент: ${student2.name} ${student2.surname}, Вік: ${student2.getAge()}`);
     student2.summary();
 
 const student3 = new Student('Олег', 'Гриненко', 1993, [5, 2, 3, 4, 5]);
-    for (let i = 0; i < 5; i++) student3.present();
-    for (let i = 0; i < 20; i++) student3.absent();
+    for (let i = 0; i < 5; i++) {
+        student3.present();
+    }
+    for (let i = 0; i < 20; i++) {
+        student3.absent();
+    }
     alert(`Студент: ${student3.name} ${student3.surname}, Вік: ${student3.getAge()}`);
     student3.summary();
