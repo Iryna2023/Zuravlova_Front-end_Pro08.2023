82d304fabd513dba0674f408d0fda001f9905919
Ірина Журавльова <irina5275z@gmail.com>
2023-09-24 21:48:51 +0300

    Changes have been made to page Homework 16. User roles.

--- front-end_Pro_d.z_16/js/form.js
+++ front-end_Pro_d.z_16/js/form.js
@@ -1,26 +1,80 @@
-const Roles = {
-    Admin: {
-      name: 'Admin',
-      permissions: ['readonly', 'edit', 'change_role']
-    },
-
-    Editor: {
-      name: 'Editor',
-      permissions: ['readonly', 'edit']
-    },
-
-    Reader: {
-      name: 'Reader',
-      permissions: ['readonly']
-    },
-
-    SuperAdmin: {
-      name: 'SuperAdmin',
-      permissions: ['readonly', 'edit', 'change_role', 'add_admin']
-    }
-};
+function deepFreeze(object) {
+    let propertyNames = Object.getOwnPropertyNames(object);
   
+    propertyNames.forEach(function(name) {
+      let prop = object[name];
+  
+      if (typeof prop == 'object' && prop !== null)
+        deepFreeze(prop);
+    });
+  
+    return Object.freeze(object);
+}
+  
+const Roles = {};
+
+Object.defineProperty(Roles, 'Admin', {
+    value: deepFreeze ({
+        name: 'admin',
+        Permissions: {
+            description: 'має доступ до всього контенту, може читати (readonly) та змінювати (edit) контент та змінювати ролі інших користувачів на Editor',
+            canRead: true,
+            canEdit: true,
+            canChangeRole: true,
+            canAddAdmin: false
+        }
+    }),
+    writable: false
+});
+
+Object.defineProperty(Roles, 'Editor', {
+    value: deepFreeze ({
+        name: 'editor',
+        Permissions: {
+            description: 'може читати та змінювати контент',
+            canRead: true,
+            canEdit: true,
+            canChangeRole: false,
+            canAddAdmin: false
+        }
+    }),
+    writable: false
+});  
+
+Object.defineProperty(Roles, 'Reader', {
+    value: deepFreeze ({
+        name: 'reader',
+        Permissions: {
+            description: 'може тільки читати контент',
+            canRead: true,
+            canEdit: false,
+            canChangeRole: false,
+            canAddAdmin: false
+        }
+    }),
+    writable: false
+});
+
+Object.defineProperty(Roles, 'SuperAdmin', {
+    value: deepFreeze ({
+        name: 'superAdmin',
+        Permissions: {
+            description: 'може все, що Admin, та додатково додавати роль Admin користувачам',
+            canRead: true,
+            canEdit: true,
+            canChangeRole: true,
+            canAddAdmin: true
+        }
+    }),
+    writable: false
+});
+
 Object.freeze(Roles);
+
+alert(JSON.stringify(Roles.Admin, null, 2));
+alert(JSON.stringify(Roles.Editor, null, 2));
+alert(JSON.stringify(Roles.Reader, null, 2));
+alert(JSON.stringify(Roles.SuperAdmin, null, 2));
   
 class User {
     constructor(id, firstName, lastName, role) {
@@ -33,9 +87,10 @@ class User {
         this.lastName = lastName;
     
         Object.defineProperty(this, 'role', {
-            value: role,
+            value: deepFreeze (role),
             writable: false,
-            configurable: false
+            configurable: false,
+            enumerable: true
         });
     }
 }
@@ -45,13 +100,9 @@ function isValidRole(role) {
 }
 
 const user1 = new User(1, 'John', 'Doe', Roles.Admin);
-alert(JSON.stringify(isValidRole(user1.role)));
-user1.role === Roles.Admin;
-user1.role = false;
-delete user1.role;
+(JSON.stringify(isValidRole(user1.role)));
+alert(user1.role === Roles.Admin);
 
 const user2 = new User(2, 'Jane', 'Smith', Roles.Editor);
-alert(JSON.stringify(isValidRole(user2.role)));
-user2 === Roles.Editor;
-user2.role = false;
-delete user2.role;
+(JSON.stringify(isValidRole(user2.role)));
+alert(user2.role === Roles.Editor);
