0c4706b2c1f103f511a21cd64d9a4284a26eb6a4
Ірина Журавльова <irina5275z@gmail.com>
2023-10-22 17:47:57 +0300

    The toCamelCase function was written on the page Homework 24. The toCamelCase function.

---
+++ front-end_Pro_d.z_24/js/index.js
@@ -0,0 +1,22 @@
+function toCamelCase(str) {
+    if (typeof str !== 'string' || !str.includes('_')) {
+        return str;
+    }
+
+    return str.replace(/_([a-z])/g, function (g) { return g[1].toUpperCase(); });
+}
+
+const response = {
+    user_id: 'abc123',
+    company_name: 'Hillel',
+    contract_expiration_date: '22/11/2013',
+    'secret-token': 'unique$ecret',
+};
+  
+let camelCasedResponse = Object.keys(response).reduce((result, key) => {
+    const newKey = toCamelCase(key);
+    result[newKey] = response[key];
+    return result;
+}, {});
+  
+document.getElementById('output').textContent = JSON.stringify(camelCasedResponse, null, 2);
